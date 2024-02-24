6e0cc07bbde3319ff76ad05c8ef24232ffdc0485
dario-99 <dariodimeo9999@gmail.com>
2024-02-24 19:41:36 +0100

    added inverted index

--- loader.py
+++ loader.py
@@ -7,7 +7,8 @@ Input:
         - Second line contains integer 1<= D <= 5, followed by names of ingredients a client dislikes, delimited by spaces
 """
 
-import os 
+import os
+from collections import defaultdict
 
 class Loader:
     def __init__(self, path):
@@ -16,22 +17,33 @@ class Loader:
         with open(path) as f:
             self.num_clients = int(f.readline().strip())
             self.clients_vector = []
-            for _ in range(self.num_clients):
+            self.ingredients = defaultdict(lambda: [0,0])
+            self.inverted_index = defaultdict(lambda: [[], []])
+            for c in range(self.num_clients):
                 like_tmp = f.readline().strip().split(" ")
                 L = int(like_tmp[0])
                 liked = like_tmp[1::]
+                for ing in liked:
+                    self.ingredients[ing][0] +=1
+                    self.inverted_index[ing][0].append(c)
                 
                 dislike_tmp = f.readline().strip().split(" ")
                 D = int(dislike_tmp[0])
                 disliked = dislike_tmp[1::]
+                for ing in disliked:
+                    self.ingredients[ing][1] +=1
+                    self.inverted_index[ing][1].append(c)
                 
                 self.clients_vector.append({"liked":liked, "disliked":disliked, "L":L, "D":D})
                 
-    def print(self):
-        print(self.clients_vector)
+    def print(self, verbose=False):
+        print(self.ingredients)
+        print(self.inverted_index)
+        if verbose: 
+            print(self.clients_vector)
         
 
 if __name__ == "__main__":
-    load = Loader("./dataset/e_elaborate.in.txt")
+    load = Loader("./dataset/a_an_example.in.txt")
     load.print()
 
